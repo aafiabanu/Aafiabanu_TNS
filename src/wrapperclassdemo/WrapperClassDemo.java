d1f55d6c94ca645871792a6bfda95ef4d1c90049
OWNER <OWNER@172.16.150.76>
2025-08-11 15:03:50 +0530

    java

---
+++ src/wrapperclassdemo/WrapperClassDemo.java
@@ -0,0 +1,19 @@
+package wrapperclassdemo;
+
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+				int x = 5;
+				Integer y = Integer.valueOf(x);
+				 
+				
+				int z = y;
+				
+				System.out.println(y +" "+ z);
+
+	}
+
+}
